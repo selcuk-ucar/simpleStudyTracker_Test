7ebf3a6933e7e073397f78b8f1c25ccec59319a2
MoeRon2 <fbml421@gmail.com>
2025-12-23 12:52:33 +0300

    timer added to dist

--- src/App.tsx
+++ src/App.tsx
@@ -34,52 +34,8 @@ function App() {
   const [importText, setImportText] = useState<string>("");
   const [category, setCategory] = useState<string>("");
   const [copied, setCopied] = useState(false);
-  const [timeLeft, setTimeLeft] = useState<string>("");
 
-  // calculate countdown until Dec 5, 2025
-  const [expiryTime, setExpiryTime] = useState("15 jul 2022 18:00:00");
-  const [countdownTime, setCountdownTime] = useState({
-    countdownDays: "",
-    countdownHours: "",
-    countdownlMinutes: "",
-    countdownSeconds: "",
-  });
 
-  const countdownTimer = () => {
-    const timeInterval = setInterval(() => {
-      const countdownDateTime = new Date(expiryTime).getTime();
-      const currentTime = new Date().getTime();
-      const remainingDayTime = countdownDateTime - currentTime;
-      const totalDays = Math.floor(remainingDayTime / (1000 * 60 * 60 * 24));
-      const totalHours = Math.floor(
-        (remainingDayTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const totalMinutes = Math.floor(
-        (remainingDayTime % (1000 * 60 * 60)) / (1000 * 60)
-      );
-      const totalSeconds = Math.floor(
-        (remainingDayTime % (1000 * 60)) / 1000
-      );
-
-      const runningCountdownTime = {
-        countdownDays: totalDays,
-        countdownHours: totalHours,
-        countdownMinutes: totalMinutes,
-        countdownSeconds: totalSeconds,
-      };
-
-      setCountdownTime(runningCountdownTime);
-
-      if (remainingDayTime < 0) {
-        clearInterval(timeInterval);
-        setExpiryTime(false);
-      }
-    }, 1000);
-  };
-
-  useEffect(() => {
-    countdownTimer();
-  });
   const changeDone = (clickedTaskId: string) => {
 
     const newTasks = tasks.map(task =>
